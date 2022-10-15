b5639b72b42ac7b0dee121a0bf36bcbaae148ffd
Oleg Adamov <olegadamov84@gmail.com>
2022-10-15 17:25:57 +0300

    package

---
+++ src/theme.js
@@ -0,0 +1,21 @@
+export const theme = {
+  colors: {
+    text: '#000',
+    background: '#FFFFFF',
+    primary: '#07c',
+    secondary: '#05a',
+    accent: '#609',
+    muted: '#f6f6f6',
+    black: 'black',
+    white: 'white',
+    red: 'red',
+    green: 'green',
+    seawave: '#09edc7',
+    magenta: '#e9a4f5',
+  },
+  radii: {
+    none: '0',
+    normal: '4px',
+    round: '50%',
+  },
+};

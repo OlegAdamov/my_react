201965644980d4bd1dbc4e6994b2df7e04505933
Oleg Adamov <olegadamov84@gmail.com>
2022-10-24 17:14:44 +0300

    Counter from Repeta

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import PaintingList from './PaintingList/PaintingList';
+import paintings from './PaintingList/paintings.json';
+import { EventBoard } from './EventBoard/EventBoard';
+import { PageTitle } from './PageTitle/PageTitle';
+import Section from './Section';
+import upcomingEvents from './upcoming-events.json';
+import Counter from './Counter/Counter';
+
+export default function App() {
+  return (
+    <>
+      <Section title="My React">
+        <PaintingList items={paintings} />
+        <PageTitle text="24th Core Worlds Coalition Conference" />
+        <EventBoard events={upcomingEvents} />
+        <Counter initialValue={0} />
+      </Section>
+    </>
+  );
+}

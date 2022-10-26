a786b4f6bace1356185b0ecb677a6b436a8b0fe9
Oleg Adamov <olegadamov84@gmail.com>
2022-10-26 21:13:56 +0300

    Some changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,25 @@
-import PaintingList from '../PaintingList/PaintingList';
-import paintings from '../PaintingList/paintings.json';
-import { EventBoard } from '../EventBoard/EventBoard';
-import { PageTitle } from '../PageTitle/PageTitle';
-import Section from '../Section';
-import upcomingEvents from '../upcoming-events.json';
-import Counter from '../Counter/Counter';
-import { Container } from './App.styled';
+// import PaintingList from '../PaintingList/PaintingList';
+// import paintings from '../PaintingList/paintings.json';
+// import { EventBoard } from '../EventBoard/EventBoard';
+// import { PageTitle } from '../PageTitle/PageTitle';
+// import Section from '../Section';
+// import upcomingEvents from '../upcoming-events.json';
+// import Counter from '../Counter/Counter';
+// import { Container } from './App.styled';
+import Dropdown from 'components/Dropdown/Dropdown';
 
 export default function App() {
   return (
     <>
-      <Section title="My React">
+      {/* <Section title="My React">
         <PaintingList items={paintings} />
         <Counter initialValue={0} />
       </Section>
       <Container>
         <PageTitle text="24th Core Worlds Coalition Conference" />
         <EventBoard events={upcomingEvents} />
-      </Container>
+      </Container> */}
+      <Dropdown />
     </>
   );
 }
---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,35 @@
+import { isThisISOWeek } from 'date-fns';
+import React, { Component } from 'react';
+import './Dropdown.css';
+
+class Dropdown extends Component {
+  state = { visible: false };
+
+  show = () => {
+    this.setState({ visible: true });
+  };
+
+  hide = () => {
+    this.setState({ visible: false });
+  };
+
+  render() {
+    return (
+      <div className="Dropdown">
+        <button type="button" className="Dropdown__toggle" onClick={this.show}>
+          Показать
+        </button>
+
+        <button type="button" className="Dropdown__toggle" onClick={this.hide}>
+          Скрыть
+        </button>
+
+        {this.state.visible && (
+          <div className="Dropdown__menu">Выпадающее меню</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Dropdown;

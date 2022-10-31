92804e21d10fe0b04ef6532d7a4c8f33d66a246e
Oleg Adamov <olegadamov84@gmail.com>
2022-10-31 18:08:26 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,20 +6,9 @@ import React from 'react';
 import Section from '../Section';
 import { HiArchive, HiArrowCircleUp, HiBriefcase } from 'react-icons/hi';
 // import upcomingEvents from '../upcoming-events.json';
-// import Counter from '../Counter/Counter';
 import { Container } from './App.styled';
 import { Button } from './Button';
 import { Box } from 'components/Box';
-// import Dropdown from 'components/Dropdown/Dropdown';
-
-// const colorPickerOptions = [
-//   { label: 'red', color: '#F44336' },
-//   { label: 'green', color: '#4CAF50' },
-//   { label: 'blue', color: '#2196F3' },
-//   { label: 'grey', color: '#607D8B' },
-//   { label: 'pink', color: '#E91E63' },
-//   { label: 'indigo', color: '#3F51B5' },
-// ];
 
 export default function App() {
   return (
@@ -33,8 +22,6 @@ export default function App() {
         {/* <PageTitle text="24th Core Worlds Coalition Conference" /> */}
         {/* <EventBoard events={upcomingEvents} /> */}
       </Container>
-      {/* <Dropdown /> */}
-      {/* <ColorPicker options={colorPickerOptions} /> */}
       <Button icon={HiArchive}>Search</Button>
       <Button icon={HiArrowCircleUp}>Filter</Button>
       <Button icon={HiBriefcase}>Show Modal</Button>
